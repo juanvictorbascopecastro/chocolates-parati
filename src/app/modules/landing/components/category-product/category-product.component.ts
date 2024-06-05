fe453d35783853bfb1cf0c84e20de5fa72a0ccd2
Victor Bascope <juan.victor.bascope.castro@gmail.com>
2024-06-05 18:53:56 -0400

    configurando el proyecto

---
+++ src/app/modules/landing/components/category-product/category-product.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-category-product',
+  templateUrl: './category-product.component.html',
+  styleUrl: './category-product.component.css',
+})
+export default class CategoryProductComponent {
+  public itemActive: number = 1;
+}
---
+++ src/app/modules/landing/landing.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import CategoryProductComponent from './components/category-product/category-product.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [CategoryProductComponent],
+  exports: [],
+})
+export class LandingModule {}
